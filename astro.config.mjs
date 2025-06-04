714eb4a0127ac83e758fa89e5e93f949f40a834b
Strahil Peykov <strahil.peykov@gmail.com>
2025-06-04 17:43:14 +0200

    www

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rotorem.bg',
+  site: 'https://www.rotorem.bg',
   integrations: [
     tailwind({
       // Using a simple config without requiring daisyui directly 
