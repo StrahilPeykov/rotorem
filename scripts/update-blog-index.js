717805c907c67971bc84a7103f1d8086b39a124a
Doroteya <doroteya.garbachkova@netinfo.bg>
2025-12-09 22:54:39 +0200

    blog 4 and 4 rows

--- scripts/update-blog-index.js
+++ scripts/update-blog-index.js
@@ -292,7 +292,7 @@ ${blogPostsArray}
         overflow: hidden;
         display: -webkit-box;
         -webkit-box-orient: vertical;
-        -webkit-line-clamp: 3;
+        -webkit-line-clamp: 4;
       }
     </style>
     
